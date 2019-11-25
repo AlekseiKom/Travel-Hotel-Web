656729c224974a1c6de4fe064858d89cdb359f7d
AlekseiKom <53940821+AlekseiKom@users.noreply.github.com>
2019-11-25 17:18:41 +0200

    Created

---
+++ JS/JS.js
@@ -0,0 +1,8 @@
+/* $(document).ready(function() {
+    var freqSecs = 1.2;
+    setInterval (blink, freqSecs*1000 );
+    function blink() {
+     var inout = (freqSecs*1000)/0.5;
+     $("#ask").fadeIn(inout).fadeOut(inout);
+    }
+   }); */
